6690a824b9d54cc6acf3891441c9c474b6bdf132
Miguel <angelgarrix14@gmail.com>
2026-01-14 04:58:30 -0500

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,7 +1,8 @@
 <?php
 	//Autor: Miguel Justiniano <angelgarrix14@gmail.com>
-	// El nombre por defecto es Mundo
-	$nombre = isset($argv[1]) ? $argv[1] : "Mundo"; 
-	@print "Hola {$argv[1]}\n"; 
+	
+	print "Introduce tu nombre:";
+	$nombre = trim(fgets(STDIN));
+	@print "Hola, {$nombre}\n"; 
 
 ?>
